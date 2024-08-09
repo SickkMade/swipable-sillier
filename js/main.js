7e6331d827465cc031b19d706b2dd3b5031af087
sickk <jaakmyr@gmail.com>
2024-08-08 21:54:14 -0700

    made smooth like butter... criminal under cover. then imagine the dancing emohi but idk how to do that on this computer

--- js/main.js
+++ js/main.js
@@ -21,24 +21,26 @@ document.querySelectorAll('.swipable').forEach(item => {
         item.style.transform = `rotate(${0}deg) translate(-50%, -50%)`
     })
     document.addEventListener('mousemove', event => {
-        if(mouseDownSwipable){
-            item.style.left = event.clientX - clickLocation +"px"
-            item.style.transform = `scale(110%) translate(-50%, -45%) rotate(${Math.min(15 * mouseSpeed, 100) * (event.clientX - lastMouseX < 0 ? -1 : 1) + (startX - event.clientX)/50}deg)`
-        }
-
-        if (lastMouseX !== null && lastMouseY !== null && lastTime !== null) {
-            let deltaX = event.clientX - lastMouseX;
-            let deltaY = event.clientY - lastMouseY;
+        requestAnimationFrame(() => {
+            if(mouseDownSwipable){
+                item.style.left = event.clientX - clickLocation +"px"
+                item.style.transform = `scale(110%) translate(-50%, -45%) rotate(${Math.min(15 * mouseSpeed, 100) * (event.clientX - lastMouseX < 0 ? -1 : 1) + (startX - event.clientX)/50}deg)`
+            }
     
-            let distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-            let timeDelta = Date.now() - lastTime;
-    
-            // mouse speed per milisecond :D
-            mouseSpeed =(distance / timeDelta);
-        }
-    
-        lastMouseX = event.clientX;
-        lastMouseY = event.clientY;
-        lastTime = Date.now();
+            if (lastMouseX !== null && lastMouseY !== null && lastTime !== null) {
+                let deltaX = event.clientX - lastMouseX;
+                let deltaY = event.clientY - lastMouseY;
+        
+                let distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+                let timeDelta = Date.now() - lastTime;
+        
+                // mouse speed per milisecond :D
+                mouseSpeed =(distance / timeDelta);
+            }
+        
+            lastMouseX = event.clientX;
+            lastMouseY = event.clientY;
+            lastTime = Date.now();
+        })
     })
 })
