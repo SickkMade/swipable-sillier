11cf66d40cc0374090efde5e16ca1c7c332a9217
sickk <jaakmyr@gmail.com>
2024-08-08 18:31:22 -0700

    i am a math god

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@ let clickLocation = 0
 let startX
 let mouseDownSwipable = false
 
+let lastMouseX = null;
+let lastMouseY = null;
+let lastTime = null;
+
+let mouseSpeed = 0;
+
 document.querySelectorAll('.swipable').forEach(item => {
     item.addEventListener('mousedown', event => {
         startX = parseInt(item.offsetLeft)
@@ -18,8 +24,22 @@ document.querySelectorAll('.swipable').forEach(item => {
     document.addEventListener('mousemove', event => {
         if(mouseDownSwipable){
             item.style.left = event.clientX - clickLocation +"px"
-            
-            item.style.transform = `translate(-50%, -50%) rotate(${(startX - event.clientX)/25}deg)`
+            item.style.transform = `translate(-50%, -50%) rotate(${20 * mouseSpeed + (startX - event.clientX)/25}deg)`
         }
+
+        if (lastMouseX !== null && lastMouseY !== null && lastTime !== null) {
+            let deltaX = event.clientX - lastMouseX;
+            let deltaY = event.clientY - lastMouseY;
+    
+            let distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+            let timeDelta = Date.now() - lastTime;
+    
+            // Speed in pixels per millisecond
+            mouseSpeed =(distance / timeDelta) * (deltaX < 0 ? -1 : 1);
+        }
+    
+        lastMouseX = event.clientX;
+        lastMouseY = event.clientY;
+        lastTime = Date.now();
     })
 })
