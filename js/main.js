21207e30729d31bfe09e970286f92624c8a00c29
sickk <jaakmyr@gmail.com>
2024-08-08 18:15:47 -0700

    i just have to add speed direction to rotation

--- js/main.js
+++ js/main.js
@@ -5,12 +5,12 @@ let mouseDownSwipable = false
 document.querySelectorAll('.swipable').forEach(item => {
     item.addEventListener('mousedown', event => {
         startX = parseInt(item.offsetLeft)
-        item.setCapture();
+        item.setPointerCapture(event.pointerId);
         clickLocation = event.clientX - startX
         mouseDownSwipable = true
     })
-    item.addEventListener('mouseup', () => {
-        document.releaseCapture();
+    item.addEventListener('mouseup', event => {
+        item.releasePointerCapture(event.pointerId);
         mouseDownSwipable = false
         item.style.left = 50+"%"
         item.style.transform = `rotate(${0}deg) translate(-50%, -50%)`
@@ -18,6 +18,7 @@ document.querySelectorAll('.swipable').forEach(item => {
     document.addEventListener('mousemove', event => {
         if(mouseDownSwipable){
             item.style.left = event.clientX - clickLocation +"px"
+            
             item.style.transform = `translate(-50%, -50%) rotate(${(startX - event.clientX)/25}deg)`
         }
     })
