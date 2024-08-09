59ef49d59da293e603a14511d01a3eb5367d6430
sickk <jaakmyr@gmail.com>
2024-08-08 21:50:47 -0700

    made 8000% smoother :sob: thank you requestanimationframe

--- js/main.js
+++ js/main.js
@@ -24,24 +24,27 @@ document.querySelectorAll('.swipable').forEach(item => {
     })
     document.addEventListener('mousemove', event => {
         if(mouseDownSwipable){
-            item.style.left = event.clientX - clickLocation +"px"
-            item.style.boxShadow = `${(item.offsetLeft-startX)/75}px 2px 20px rgba(0, 0, 0, 0.5)`
-            item.children[0].style.textShadow = `${(item.offsetLeft-startX)/250}px 2px 2px rgba(0, 0, 0, 0.5)`
-            item.style.transform = `scale(110%) translate(-50%, ${-45 + (Math.abs(startX - event.clientX)/25)}%) rotate(${ (startX - event.clientX)/-75}deg) skew(${ (startX - event.clientX)/-100}deg, ${(startX - event.clientX)/-100}deg)`
-        
-
-            if (lastMouseX !== null && lastMouseY !== null && lastTime !== null) {
-                let deltaX = event.clientX - lastMouseX;
-                let deltaY = event.clientY - lastMouseY;
-        
-                let distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-        
-                requestAnimationFrame(() => mouseSpeed = distance)
-            }
-        
-            lastMouseX = event.clientX;
-            lastMouseY = event.clientY;
-            lastTime = Date.now();
+            requestAnimationFrame(() => {
+                item.style.left = event.clientX - clickLocation +"px"
+                item.style.boxShadow = `${(item.offsetLeft-startX)/75}px 2px 20px rgba(0, 0, 0, 0.5)`
+                item.children[0].style.textShadow = `${(item.offsetLeft-startX)/250}px 2px 2px rgba(0, 0, 0, 0.5)`
+                item.style.transform = `scale(110%) translate(-50%, ${-45 + (Math.abs(startX - event.clientX)/25)}%) rotate(${ (startX - event.clientX)/-75}deg) skew(${ (startX - event.clientX)/-100}deg, ${(startX - event.clientX)/-100}deg)`
+            
+    
+                if (lastMouseX !== null && lastMouseY !== null && lastTime !== null) {
+                    let deltaX = event.clientX - lastMouseX;
+                    let deltaY = event.clientY - lastMouseY;
+            
+                    let distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+            
+                    mouseSpeed = distance
+                }
+            
+                lastMouseX = event.clientX;
+                lastMouseY = event.clientY;
+                lastTime = Date.now();
+            })
+            
         }
     })
 })
